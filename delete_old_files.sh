49d32b3d2dec50006470d5e1b0fdf6bf93f6cf81
glazkov.a <glazkov.a@incoel.ru>
2020-02-18 12:11:19 +0300

    10

--- delete_old_files.sh
+++ delete_old_files.sh
@@ -33,7 +33,7 @@ blocks_full=`stat -f --format=%b "$1"`
 #current decimal
 par_ex=`bc <<< "100*$blocks_free/$blocks_full"`
 
-systemd-cat -t DelOld echo "$par_ex"
+systemd-cat -t DelOld echo Current usage "$par_ex"
 
 if [ "$par_ex" -ge "$par_fr" ]; then
 echo "No action"
@@ -42,19 +42,11 @@ fi
 
 until [ "$par_ex" -ge "$par_fr" ]
 do
-#echo `find "$1" -name '*.'"$2" -type f -print0 | xargs -0 stat -c "%Y %N" | sort -n | head -n 1 | cut -s -d ' ' -f 2`
 find "$1" -name '*.'"$2" -type f -print0 | xargs -0 stat -c "%Y %N" | sort -n | head -n 1 | cut -s -d ' ' -f 2 | xargs rm
-
-#echo "$fname"
-#if [ -e "$fname" ]; then
-#systemd-cat -t DelOld echo "$fname"
-#rm -fv $fname
-#fi
 #get free space
 blocks_free=$(stat -f --format=%a "$1")
 #current decimal
 par_ex=$(bc <<< "100*$blocks_free/$blocks_full")
-#systemd-cat -t DelOld echo "$par_ex"
 done
 
 exit
